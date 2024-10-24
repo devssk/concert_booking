52e3e983434212b7766e1cd5e5ea621592414d40
devssk <sksong419@naver.com>
2024-10-25 01:08:09 +0900

    fix : 특정 에러 코드 httpstatus 변경

--- src/main/java/io/concert_booking/common/exception/ErrorCode.java
+++ src/main/java/io/concert_booking/common/exception/ErrorCode.java
@@ -9,8 +9,8 @@ public enum ErrorCode {
     NOT_FOUND_ENTITY(HttpStatus.BAD_REQUEST, "해당 ID로 찾을 수 없음"),
     NOT_FOUND_QUEUE(HttpStatus.BAD_REQUEST, "대기열에서 찾을 수 없음"),
     OCCUPANCY_SEAT(HttpStatus.BAD_REQUEST, "이미 결제 대기중인 좌석"),
-    NOT_ENTER_TOKEN(HttpStatus.BAD_REQUEST, "대기열 통과되지 않은 토큰"),
-    NOT_OCCUPANCY_TOKEN(HttpStatus.BAD_REQUEST, "좌석을 점유하지 않은 토큰"),
+    NOT_ENTER_TOKEN(HttpStatus.FORBIDDEN, "대기열 통과되지 않은 토큰"),
+    NOT_OCCUPANCY_TOKEN(HttpStatus.FORBIDDEN, "좌석을 점유하지 않은 토큰"),
     EXPIRED_OCCUPANCY_SEAT(HttpStatus.BAD_REQUEST, "좌석 점유 시간 만료"),
     NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액 부족")
     ;
