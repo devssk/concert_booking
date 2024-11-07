80055a359342a190e51719408532a7b7a24900e3
devssk <sksong419@naver.com>
2024-11-08 03:36:03 +0900

    feat : domain단 QueueService 변경에 따른 interfaces단 Scheduler 수정

--- src/main/java/io/concert_booking/interfaces/scheduler/QueueScheduler.java
+++ src/main/java/io/concert_booking/interfaces/scheduler/QueueScheduler.java
@@ -1,10 +1,12 @@
 package io.concert_booking.interfaces.scheduler;
 
+import io.concert_booking.domain.concert.service.ConcertInfoService;
 import io.concert_booking.domain.queue.service.QueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -12,20 +14,15 @@ import java.util.List;
 public class QueueScheduler {
 
     private final QueueService queueService;
+    private final ConcertInfoService concertInfoService;
 
-    @Scheduled(fixedDelay = 60000)
+    @Scheduled(fixedDelay = 5000)
     public void passQueueScheduler() {
         int passCount = 50;
-        List<Long> queueConcertIdList = queueService.getQueueConcertIdList();
-        for (Long concertId : queueConcertIdList) {
+        List<Long> todayConcertIdList = concertInfoService.getAllCocnertIdByConcertDate(LocalDate.now());
+        for (Long concertId : todayConcertIdList) {
             queueService.passQueue(concertId, passCount);
         }
-    }
-
-    @Scheduled(fixedDelay = 60000)
-    public void getOutQueueScheduler() {
-        int expiredMinute = 20;
-        queueService.getOutQueue(expiredMinute);
     }
 
 }
